8918c84a84d1fcd61ffd30ad6e6e49229075d306
Andres Fredes <andres.hector.fredes@gmail.com>
2021-08-18 10:27:17 +0930

    Added config.py with env vars

--- commit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-read -p "Commit comment: " comment
-git add -A
-git commit -m "\"$comment\""
-git push
--- prices/__init__.py
+++ prices/__init__.py
@@ -1,12 +1,22 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+import config
 
 bootstrap = Bootstrap()
 
 def create_app():
     app = Flask(__name__)
+    load_config(app)
     bootstrap.init_app(app)
     return app
+
+def load_config(app):
+    if app.config["ENV"] == "production":
+        app.config.from_object(config.ProductionConfig)
+    elif app.config["ENV"] == "testing":
+        app.config.from_object(config.TestingConfig)
+    else:
+        app.config.from_object(config.DevelopmentConfig)
 
 app = create_app()
 
